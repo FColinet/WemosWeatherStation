03a42fdf2ac4bfe77fbe79a98b2f0782bc10ae61
Florent Colinet <colinetflorent@gmail.com>
2017-10-21 13:58:51 +0200

    clean .ino

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -46,31 +46,6 @@ void setup() {
 
 void loop() {
   webserver.handleClient();
-  /*
-  Serial.print("Temperature    ");
-  Serial.println(ws.thermometer());
-  Serial.print("Humidity       ");
-  Serial.println(ws.hygrometer());
-  Serial.print("Pressure       ");
-  Serial.println(ws.atmospheric_pressure());
-  Serial.print("Altitude       ");
-  Serial.println(ws.altimeter());
-  Serial.print("Dew point      ");
-  Serial.println(ws.dew_point());
-  Serial.print("Wind speed     ");
-  Serial.println(ws.anemometer());
-  Serial.print("Wind direction ");
-  Serial.println(ws.weathercock());
-  Serial.print("Rain quantity  ");
-  Serial.println(ws.rain_gauge());
-  Serial.println("");
-  Serial.print("Voltage        ");
-  Serial.println(ws.voltage());
-  Serial.print("Wifi strength  ");
-  Serial.println(ws.wifi_strength());
-  Serial.println("");
-  delay(10000);
-  */
 }
 
 void handleRoot() {
